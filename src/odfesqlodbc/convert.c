76d5c336bf6256af5a03859032f3b11493419636
Jerry Leung <jerryl@bitquilltech.com>
2021-03-03 14:44:12 -0800

    Support Aws::TimestreamQuery::Model::ScalarType::TIMESTAMP

--- src/odfesqlodbc/convert.c
+++ src/odfesqlodbc/convert.c
@@ -1112,8 +1112,8 @@ int copy_and_convert_field(StatementClass *stmt, OID field_type, int atttypmod,
 
         case ES_TYPE_ABSTIME:
         case ES_TYPE_DATETIME:
-        case ES_TYPE_TIMESTAMP_NO_TMZONE:
-        case ES_TYPE_TIMESTAMP:
+        case TS_TYPE_TIMESTAMP_NO_TMZONE:
+        case TS_TYPE_TIMESTAMP:
             std_time.fr = 0;
             std_time.infinity = 0;
             if (strnicmp(value, INFINITY_STRING, 8) == 0) {
@@ -1136,7 +1136,7 @@ int copy_and_convert_field(StatementClass *stmt, OID field_type, int atttypmod,
                 std_time.ss = 0;
             }
             if (strnicmp(value, "invalid", 7) != 0) {
-                BOOL bZone = field_type != ES_TYPE_TIMESTAMP_NO_TMZONE;
+                BOOL bZone = field_type != TS_TYPE_TIMESTAMP_NO_TMZONE;
                 int zone;
 
                 /*
@@ -1323,8 +1323,8 @@ int copy_and_convert_field(StatementClass *stmt, OID field_type, int atttypmod,
 
             case ES_TYPE_ABSTIME:
             case ES_TYPE_DATETIME:
-            case ES_TYPE_TIMESTAMP_NO_TMZONE:
-            case ES_TYPE_TIMESTAMP:
+            case TS_TYPE_TIMESTAMP_NO_TMZONE:
+            case TS_TYPE_TIMESTAMP:
                 len = stime2timestamp(&std_time, midtemp, midsize, FALSE,
                                       (int)(midsize - 19 - 2));
                 break;
