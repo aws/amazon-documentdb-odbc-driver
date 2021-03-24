4d30f681cce226ad85f260b03277f91dd4c69338
Jerry Leung <jerryl@bitquilltech.com>
2021-03-24 11:27:39 -0700

    Added test cases for SQLExecute

--- src/odfesqlodbc/es_apifunc.h
+++ src/odfesqlodbc/es_apifunc.h
@@ -79,7 +79,7 @@ RETCODE SQL_API ESAPI_StmtError(HSTMT StatementHandle, SQLSMALLINT RecNumber,
 RETCODE SQL_API ESAPI_ExecDirect(HSTMT StatementHandle,
                                  const SQLCHAR *StatementText,
                                  SQLINTEGER TextLength, BOOL commit);
-RETCODE SQL_API ESAPI_Execute(HSTMT StatementHandle);
+RETCODE SQL_API API_Execute(HSTMT StatementHandle);
 RETCODE SQL_API ESAPI_Fetch(HSTMT StatementHandle);
 RETCODE SQL_API ESAPI_FreeConnect(HDBC ConnectionHandle);
 RETCODE SQL_API ESAPI_FreeEnv(HENV EnvironmentHandle);
