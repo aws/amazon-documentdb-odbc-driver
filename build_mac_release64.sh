e39f09d5df2f68eeee572f21b53b4191d37b3a36
Yan Wang <68562925+yanw-bq@users.noreply.github.com>
2021-04-29 10:12:51 -0700

    At 706 - AAD support (#53)
    * get access token
    
    * add libcurl dependency
    
    * use http client inside aws sdk
    
    * AAD support
    
    * resolve base64 decoding issue
    
    * fix format
    
    * fix indentation
    
    * remove demo from cmake
    
    * revert format change
    
    * revert format fchange
    
    * make role arn available in AAD auth mode

--- build_mac_release64.sh
+++ build_mac_release64.sh
@@ -8,7 +8,7 @@ cd ..
 PREFIX_PATH=$(pwd)
 mkdir cmake-build64
 cd cmake-build64
-cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX_PATH}/AWSSDK/ -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="core;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_RTTI="OFF" -DENABLE_TESTING="OFF"
+cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX_PATH}/AWSSDK/ -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_RTTI="OFF" -DENABLE_TESTING="OFF"
 cd ..
 
 cmake --build cmake-build64 -- -j 4
