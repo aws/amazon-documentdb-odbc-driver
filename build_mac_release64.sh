f931697913e2205a8f9292da0c6ed3b815043eeb
Jerry Leung <jerrytfleung@users.noreply.github.com>
2021-06-10 10:24:56 -0700

    Fix mac build (#79)
    * Fix
    
    * Added quotation mark

--- build_mac_release64.sh
+++ build_mac_release64.sh
@@ -7,9 +7,9 @@ cd aws-sdk-cpp
 mkdir install
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF"
+cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF"
 # Rerun to set the install prefix (https://github.com/aws/aws-sdk-cpp/issues/1156)
-cmake ../ -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF"
+cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF"
 make -j 4
 make install
 cd ../../../
@@ -17,6 +17,6 @@ cd ../../../
 PREFIX_PATH=$(pwd)
 mkdir cmake-build64
 cd cmake-build64
-cmake ../src -DCMAKE_BUILD_TYPE=Release -DBUILD_WITH_TESTS=ON -DCODE_COVERAGE=ON -DBUILD_SHARED_LIBS="OFF"
+cmake ../src -DCMAKE_BUILD_TYPE="Release" -DBUILD_WITH_TESTS="ON" -DCODE_COVERAGE="OFF" -DBUILD_SHARED_LIBS="OFF"
 make -j 4
 cd ..
