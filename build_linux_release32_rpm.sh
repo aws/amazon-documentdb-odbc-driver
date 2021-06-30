078b77a2da941f7df22806e96c6a381917fffee7
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-06-30 12:42:26 -0700

    [1] Trying to fix toolchaiun for aws.

--- build_linux_release32_rpm.sh
+++ build_linux_release32_rpm.sh
@@ -1,15 +1,17 @@
 # Build AWS SDK
 # $BITNESS=32
 
+BASEDIR="$(pwd)"
+TOOLCHAIN="${BASEDIR}/src/linux_32bit_toolchain.cmake"
 cd src
 git clone --recurse-submodules -b "1.8.186" "https://github.com/aws/aws-sdk-cpp.git"
 cd aws-sdk-cpp
 mkdir install
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DCMAKE_TOOLCHAIN_FILE="linux_32bit_toolchain.cmake"
+cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
 # Rerun to set the install prefix (https://github.com/aws/aws-sdk-cpp/issues/1156)
-cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_TOOLCHAIN_FILE="linux_32bit_toolchain.cmake"
+cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
 make -j 4
 make install
 cd ../../../
@@ -17,6 +19,6 @@ cd ../../../
 PREFIX_PATH=$(pwd)
 mkdir cmake-build32
 cd cmake-build32
-cmake ../src -DCMAKE_BUILD_TYPE="Release" -DBUILD_WITH_TESTS="ON" -DCODE_COVERAGE="OFF" -DBUILD_SHARED_LIBS="OFF" -DINSTALLER_TYPE="RPM" -DCMAKE_TOOLCHAIN_FILE="linux_32bit_toolchain.cmake"
+cmake ../src -DCMAKE_BUILD_TYPE="Release" -DBUILD_WITH_TESTS="ON" -DCODE_COVERAGE="OFF" -DBUILD_SHARED_LIBS="OFF" -DINSTALLER_TYPE="RPM" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
 make -j 4
 cd ..
