b3f4e4abb21600c5d1a5f93bdd596537dad0401b
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-06-30 13:39:25 -0700

    no message

--- src/linux_32bit_toolchain.cmake
+++ src/linux_32bit_toolchain.cmake
@@ -10,11 +10,4 @@ set(CMAKE_CXX_FLAGS -m32)
 set(CMAKE_SHARED_LINKER_FLAGS -m32)
 
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/i386-linux-gnu)
-
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH  /usr/i386-linux-gnu)cmake_minimum_required(VERSION 2.8)
