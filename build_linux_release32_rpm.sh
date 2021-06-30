2fbb1fcff95f9dbe1c8f5d1de33f5165dbab93fe
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-06-30 13:18:48 -0700

    [1] Updating target properties.

--- build_linux_release32_rpm.sh
+++ build_linux_release32_rpm.sh
@@ -3,16 +3,15 @@
 
 BASEDIR="$(pwd)"
 TOOLCHAIN="${BASEDIR}/src/linux_32bit_toolchain.cmake"
-TOOLCHAIN_AWS="${BASEDIR}/src/linux_32bit_toolchain_aws.cmake"
 cd src
 git clone --recurse-submodules -b "1.8.186" "https://github.com/aws/aws-sdk-cpp.git"
 cd aws-sdk-cpp
 mkdir install
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_AWS}"
+cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
 # Rerun to set the install prefix (https://github.com/aws/aws-sdk-cpp/issues/1156)
-cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_AWS}"
+cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
 make -j 4
 make install
 cd ../../../
