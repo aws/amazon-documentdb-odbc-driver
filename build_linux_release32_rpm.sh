3a29bf9a6ccb8a7e60e64e7d5663cff8eb4909ae
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-06-30 13:51:31 -0700

    no message

--- build_linux_release32_rpm.sh
+++ build_linux_release32_rpm.sh
@@ -1,6 +1,10 @@
 # Build AWS SDK
 # $BITNESS=32
 
+pushd /usr/lib/i386-linux-gnu/
+ln -s libssl.so.1.1  libssl.so 
+ln -s libcrypto.so.1.1 libcrypto.so
+popd
 BASEDIR="$(pwd)"
 TOOLCHAIN="${BASEDIR}/src/linux_32bit_toolchain.cmake"
 cd src
@@ -9,9 +13,9 @@ cd aws-sdk-cpp
 mkdir install
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
+cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_C_FLAGS="-m32" -DCMAKE_SHARED_LINKER_FLAGS="-m32"
 # Rerun to set the install prefix (https://github.com/aws/aws-sdk-cpp/issues/1156)
-cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}"
+cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_C_FLAGS="-m32" -DCMAKE_SHARED_LINKER_FLAGS="-m32"
 make -j 4
 make install
 cd ../../../
