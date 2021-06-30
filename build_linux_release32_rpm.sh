c9654d8f3a24eceb425c02c2cca81ef384e75e9c
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-06-30 12:20:37 -0700

    [1] Adjusting linux script.

--- build_linux_release32_rpm.sh
+++ build_linux_release32_rpm.sh
@@ -7,10 +7,9 @@ cd aws-sdk-cpp
 mkdir install
 mkdir build
 cd build
-sudo mv /usr/lib/x86_64-linux-gnu /usr/lib/x86_64-linux-gnu-temp
-cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_C_FLAGS="-m32" -DCMAKE_SHARED_LINKER_FLAGS="-m32"
+cmake ../ -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DCMAKE_TOOLCHAIN_FILE="linux_32bit_toolchain.cmake"
 # Rerun to set the install prefix (https://github.com/aws/aws-sdk-cpp/issues/1156)
-cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_C_FLAGS="-m32" -DCMAKE_SHARED_LINKER_FLAGS="-m32"
+cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Release" -DBUILD_ONLY="core;sts;timestream-query" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_TOOLCHAIN_FILE="linux_32bit_toolchain.cmake"
 make -j 4
 make install
 cd ../../../
@@ -20,5 +19,4 @@ mkdir cmake-build32
 cd cmake-build32
 cmake ../src -DCMAKE_BUILD_TYPE="Release" -DBUILD_WITH_TESTS="ON" -DCODE_COVERAGE="OFF" -DBUILD_SHARED_LIBS="OFF" -DINSTALLER_TYPE="RPM" -DCMAKE_TOOLCHAIN_FILE="linux_32bit_toolchain.cmake"
 make -j 4
-sudo mv /usr/lib/x86_64-linux-gnu-temp /usr/lib/x86_64-linux-gnu
 cd ..
