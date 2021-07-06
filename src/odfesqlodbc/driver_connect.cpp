9adb11ec021fc09b13628329846b46ecf6b120a0
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-07-05 21:39:08 -0700

    [1] Added additional debug info

--- src/odfesqlodbc/driver_connect.cpp
+++ src/odfesqlodbc/driver_connect.cpp
@@ -227,10 +227,13 @@ RETCODE ESAPI_DriverConnect(HDBC hdbc, HWND hwnd, SQLCHAR *conn_str_in,
 
     // Setup connection string
     {
+        printf("Setup conn string\n");
         const SQLRETURN return_code =
             SetupConnString(conn_str_in, conn_str_in_len, ci, conn);
-        if (return_code != SQL_SUCCESS)
+        if (return_code != SQL_SUCCESS) {
+            printf("Setup conn string failed\n");
             return return_code;
+        }
     }
 
     // Initialize version
@@ -239,16 +242,19 @@ RETCODE ESAPI_DriverConnect(HDBC hdbc, HWND hwnd, SQLCHAR *conn_str_in,
     int reqs = 0;
     int retval = 0;
     do {
+        printf("GetRequirementsAndConnect\n");
         const SQLRETURN return_code = GetRequirementsAndConnect(
             driver_completion, hwnd, ci, reqs, conn, retval);
         if (return_code != SQL_SUCCESS)
             return return_code;
 
         // Check for errors
+        printf("CheckRetVal\n");
         const std::string error_msg =
             CheckRetVal(retval, hwnd, driver_completion, reqs, ci);
 
         // If we have an error, log it and exit
+        printf("Error msg: '%s'\n", error_msg.c_str());
         if (error_msg != "") {
             CC_log_error(func, error_msg.c_str(), conn);
             return SQL_ERROR;
