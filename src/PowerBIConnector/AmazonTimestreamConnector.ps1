5e4d1a4502411084b7266d1276bc0eaa9dc3743a
csolonynka <69494372+csolonynka@users.noreply.github.com>
2021-09-30 14:33:21 -0700

    Aws timestream connector (#215)
    * update power_bi_connectors
    
    1. Added a sample Power BI connector for Azure AD (AmazonTimestreamAADConnector).
    2. Renamed the TimestreamConnector to AmazonTimestreamConnector.
    3. Update the documentation for the Power BI Connector.
    4. Updated screen shots for new naming
    
    * update_doc
    
    * add EOL
    
    Co-authored-by: Charlene Solonynka <csolonynka@dremio.com>

---
+++ src/PowerBIConnector/AmazonTimestreamConnector.ps1
@@ -0,0 +1,34 @@
+﻿$ZipFile = '.\AmazonTimestreamConnector.zip'
+$MezFile = '.\AmazonTimestreamConnector.mez'
+
+if (Test-Path -Path $ZipFile) {
+    Write-Host Removing previous AmazonTimestreamConnector.zip
+
+    Remove-Item $ZipFile
+}
+
+Write-Host Creating AmazonTimestreamConnector.m file
+
+Copy-Item '.\AmazonTimestreamConnector\AmazonTimestreamConnector.pq' '.\AmazonTimestreamConnector\AmazonTimestreamConnector.m'
+
+Write-Host Creating AmazonTimestreamConnector.zip file
+
+compress-archive -path '.\AmazonTimestreamConnector\*.png', '.\AmazonTimestreamConnector\*.pqm', '.\AmazonTimestreamConnector\*.resx', '.\AmazonTimestreamConnector\AmazonTimestreamConnector.m' -destinationpath $ZipFile -update -compressionlevel optimal
+
+if (Test-Path -Path '.\AmazonTimestreamConnector\AmazonTimestreamConnector.m') {
+    Write-Host Removing AmazonTimestreamConnector.m
+
+    Remove-Item '.\AmazonTimestreamConnector\AmazonTimestreamConnector.m'
+}
+
+if (Test-Path -Path $MezFile) {
+    Write-Host Removing previous AmazonTimestreamConnector.mez
+
+    Remove-Item $MezFile
+}
+
+Write-Host Creating AmazonTimestreamConnector.mez
+
+Move-Item $ZipFile $MezFile
+
+Write-Host done
