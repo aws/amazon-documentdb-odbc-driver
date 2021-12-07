da6534cd7de38eb4074bbf01eacfa2c6ab8649ac
Bruce Irschick <brucei@bitquilltech.com>
2021-12-07 14:49:32 -0800

    [AD-492] Debugging test_main.cpp. https:/bitquill.atlassian.net/browse/AD-492

--- src/Tests/Tests/test_main.cpp
+++ src/Tests/Tests/test_main.cpp
@@ -27,39 +27,39 @@
 // clang-format on
 
 int main(int argc, char** argv) {
-    std::cout << "Start: Line 30" << std::endl;
+    std::cerr << "Start: Line 30" << std::endl;
 #ifdef WIN32
-    std::cout << "Line 32" << std::endl;
+    std::cerr << "Line 32" << std::endl;
     // Enable CRT for detecting memory leaks
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_FILE);
-    std::cout << "Line 35" << std::endl;
+    std::cerr << "Line 35" << std::endl;
     _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-    std::cout << "Line 37" << std::endl;
+    std::cerr << "Line 37" << std::endl;
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    std::cout << "Line 39" << std::endl;
+    std::cerr << "Line 39" << std::endl;
 #endif
 #ifdef __APPLE__
     // Enable malloc logging for detecting memory leaks.
     system("export MallocStackLogging=1");
 #endif
-    std::cout << "Line 45" << std::endl;
+    std::cerr << "Line 45" << std::endl;
     testing::internal::CaptureStdout();
-    std::cout << "Line 47" << std::endl;
+    std::cerr << "Line 47" << std::endl;
     ::testing::InitGoogleTest(&argc, argv);
 
-    std::cout << "Line 50" << std::endl;
+    std::cerr << "Line 50" << std::endl;
     int failures = RUN_ALL_TESTS();
 
-    std::cout << "Line 53" << std::endl;
+    std::cerr << "Line 53" << std::endl;
     std::string output = testing::internal::GetCapturedStdout();
-    std::cout << "Line 55" << std::endl;
+    std::cerr << "Line 55" << std::endl;
     std::cout << output << std::endl;
-    std::cout << "Line 56" << std::endl;
+    std::cerr << "Line 56" << std::endl;
     std::cout << (failures ? "Not all tests passed." : "All tests passed")
               << std::endl;
-    std::cout << "Line 60" << std::endl;
+    std::cerr << "Line 60" << std::endl;
     WriteFileIfSpecified(argv, argv + argc, "-fout", output);
-    std::cout << "Line 62" << std::endl;
+    std::cerr << "Line 62" << std::endl;
 
 #ifdef __APPLE__
     // Disable malloc logging and report memory leaks
