2768c6cf4feb17bd831a9bb266b8ffb85d98ae61
Gary Atwal <garya@bitquilltech.com>
2021-12-31 01:01:21 -0800

    Add system code for win32 and apple.

--- src/PerformanceTests/performance/performance.cpp
+++ src/PerformanceTests/performance/performance.cpp
@@ -39,62 +39,78 @@
  *****************************************/
 
 int main(int argc, char* argv[]) {
-  // first argument is program name
+#ifdef WIN32
+    // Enable CRT for detecting memory leaks
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+#ifdef __APPLE__
+    // Enable malloc logging for detecting memory leaks.
+    system("export MallocStackLogging=1");
+#endif
+    // first argument is program name
 
-  // Initialize variables
-  std::string data_source_name, test_plan_file, test_results_file;
+    // Initialize variables
+    std::string data_source_name, test_plan_file, test_results_file;
 
-  if (argc == 1) {
-    // default mode
-    data_source_name = dsn_default;
-    test_plan_file = input_file;
-    test_results_file = output_file;
-  } else {
-    std::cout << "You have entered " << (argc-1) << " arguments:\n";
-    for (int i = 1; i < argc; i++) {
-      std::cout << argv[i] << "\n";
-    }
-
-    if (argc == 2) {
-      // dsn passed in
-      data_source_name = argv[1];
-      test_plan_file = input_file;
-      test_results_file = output_file;
-    } else if (argc == 3) {
-      // input and output file passed in
-      data_source_name = dsn_default;
-      test_plan_file = argv[1];
-      test_results_file = argv[2];
-    } else if (argc == 4) {
-      // input file, output file and dsn passed in
-      test_plan_file = argv[1];
-      test_results_file = argv[2];
-      data_source_name = argv[3];
+    if (argc == 1) {
+        // default mode
+        data_source_name = dsn_default;
+        test_plan_file = input_file;
+        test_results_file = output_file;
     } else {
-      std::cerr << "ERROR: invalid number of command line arguments\n";
-    }
-  }
+        std::cout << "You have entered " << (argc - 1) << " arguments:\n";
+        for (int i = 1; i < argc; i++) {
+            std::cout << argv[i] << "\n";
+        }
 
-  try {
-    // Initialize performance test runner
-    performance::PerformanceTestRunner performanceTest(
-        test_plan_file, test_results_file, data_source_name);
-
-    // Connect to test data source name
-    if (!performanceTest.setupConnection()) {
-      throw std::runtime_error(performanceTest.getErrorMsg());
+        if (argc == 2) {
+            // dsn passed in
+            data_source_name = argv[1];
+            test_plan_file = input_file;
+            test_results_file = output_file;
+        } else if (argc == 3) {
+            // input and output file passed in
+            data_source_name = dsn_default;
+            test_plan_file = argv[1];
+            test_results_file = argv[2];
+        } else if (argc == 4) {
+            // input file, output file and dsn passed in
+            test_plan_file = argv[1];
+            test_results_file = argv[2];
+            data_source_name = argv[3];
+        } else {
+            std::cerr << "ERROR: invalid number of command line arguments\n";
+        }
     }
 
-    // Read test plan csv file
-    if (!performanceTest.readPerformanceTestPlan()) {
-      throw std::runtime_error(performanceTest.getErrorMsg());
+    try {
+        // Initialize performance test runner
+        performance::PerformanceTestRunner performanceTest(
+            test_plan_file, test_results_file, data_source_name);
+
+        // Connect to test data source name
+        if (!performanceTest.setupConnection()) {
+            throw std::runtime_error(performanceTest.getErrorMsg());
+        }
+
+        // Read test plan csv file
+        if (!performanceTest.readPerformanceTestPlan()) {
+            throw std::runtime_error(performanceTest.getErrorMsg());
+        }
+
+        // Run performance test
+        if (!performanceTest.runPerformanceTestPlan()) {
+            throw std::runtime_error(performanceTest.getErrorMsg());
+        }
+    } catch (const std::runtime_error err) {
+        std::cerr << err.what() << std::endl;
     }
 
-    // Run performance test
-    if (!performanceTest.runPerformanceTestPlan()) {
-      throw std::runtime_error(performanceTest.getErrorMsg());
-    }
-  } catch (const std::runtime_error err) {
-    std::cerr << err.what() << std::endl;
-  }
+#ifdef __APPLE__
+    // Disable malloc logging and report memory leaks
+    system("unset MallocStackLogging");
+    system("leaks performance_results > leaks_performance_results");
+#endif
 }
