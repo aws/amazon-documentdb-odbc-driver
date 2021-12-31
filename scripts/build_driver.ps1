406db77394f8498a0e93082013d972cbcfe55168
Bruce Irschick <brucei@bitquilltech.com>
2021-12-31 10:51:57 -0800

    [AD-504] Apache Ignite ODBC Seed replacement * Add support for compiling tests.

--- scripts/build_driver.ps1
+++ scripts/build_driver.ps1
@@ -4,15 +4,25 @@ $SRC_DIR = $args[2]
 $BUILD_DIR = $args[3]
 $INSTALL_DIR = $args[4]
 
+if ($WIN_ARCH = "x64") {
+    $VCPKG_TARGET_TRIPLET="x64-windows"
+} else {
+    $VCPKG_TARGET_TRIPLET="x86-windows"
+}
+
 cmake -S $SRC_DIR `
     -B $BUILD_DIR `
     -A $WIN_ARCH `
     -D CMAKE_BUILD_TYPE=$CONFIGURATION `
     -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR `
-    -D WITH_TESTS=OFF `
+    -D WITH_TESTS=ON `
     -D WITH_CORE=OFF `
     -D WITH_ODBC=ON `
-    -D WITH_ODBC_MSI=ON
+    -D WITH_ODBC_MSI=ON `
+    -D VCPKG_TARGET_TRIPLET=$VCPKG_TARGET_TRIPLET `
+    -D CMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 
-# # Build Project
-cmake --build $BUILD_DIR --target install --config $CONFIGURATION --parallel 4 
+if ($?) {
+    # # Build Project
+    cmake --build $BUILD_DIR --target install --config $CONFIGURATION --parallel 4 
+}
