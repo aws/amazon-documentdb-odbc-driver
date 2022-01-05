8f940d25b2eb5bd935427fd48c6e096ab9057ba5
Bruce Irschick <brucei@bitquilltech.com>
2022-01-05 13:09:04 -0800

    [AD-504] Apache Ignite ODBC Seed replacement (#4)
    ### Summary
    
    [AD-504] Apache Ignite ODBC Seed replacement
    
    ### Description
    
    * Initial commit with support for Windows and MacOS build **without test**.
    * Seeded from [Apache Ignite](https://github.com/apache/ignite) from the path [modules/platforms/cpp](https://github.com/apache/ignite/tree/master/modules/platforms/cpp).
    
    ### Related Issue
    
    [[AD-504] Apache Ignite ODBC Seed replacement](https://bitquill.atlassian.net/browse/AD-506)
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Initial commit with support for Windows build without test.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Add support for compiling tests.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Correct GitHub workflow.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Correct GitHub workflow.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Correct GitHub workflow.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Disable building of test code.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Update .gitignore
    
    * adding mac build and workflow
    
    * temporarily disabling cppcheck
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Update for MacOS build script.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Re-enable cppcheck.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Disable cppcheck.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Add install of OpenSSL.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Set OPENSSL_ROOT_DIR for macOS.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Set OPENSSL_ROOT_DIR for macOS.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Set OPENSSL_ROOT_DIR for macOS.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Remove Window-specific method from MacOS/Linux code
    
    * [AD-504] Improve MacOS build.
    
    * [AD-504] Improve MacOS build docs.
    
    Co-authored-by: affonsov <affonsov@bitquilltech.com>

--- build_mac_debug64.sh
+++ build_mac_debug64.sh
@@ -1,19 +1,8 @@
 # Build AWS SDK
 # $BITNESS=64
 
-cd src
-git clone --recurse-submodules -b "1.9.79" "https://github.com/aws/aws-sdk-cpp.git"
-cd aws-sdk-cpp
-mkdir install
-mkdir build
-cd build
-cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DCMAKE_BUILD_TYPE="Debug" -DBUILD_ONLY="core;sts" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF" -DCPP_STANDARD="17"
-make -j 4
-make install
-cd ../../../
-
 mkdir cmake-build64
 cd cmake-build64
-cmake ../src -DCMAKE_BUILD_TYPE="Debug" -DBUILD_WITH_TESTS="ON" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DCMAKE_PREFIX_PATH="../src/aws-sdk-cpp/install"
+cmake ../src -DCMAKE_BUILD_TYPE="Debug" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="OFF" -DWITH_CORE="OFF" -DWITH_ODBC="ON"
 make ccov-all -j 4
 cd ..
