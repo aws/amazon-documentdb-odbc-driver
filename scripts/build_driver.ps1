4f0d233037260fbc5dd9b97a0905f0b62194955a
Bruce Irschick <brucei@bitquilltech.com>
2022-01-05 13:09:04 -0800

    [AD-504] Apache Ignite ODBC Seed replacement (#4)
    ### Summary
    
    [AD-504] Apache Ignite ODBC Seed replacement
    
    ### Description
    
    * Initial commit with support for Windows and MacOS build **without test**.
    * Seeded from [Apache Ignite](https://github.com/apache/ignite) from the path [modules/platforms/cpp](https://github.com/apache/ignite/tree/master/modules/platforms/cpp).
    
    ### Related Issue
    
    [[AD-504] Apache Ignite ODBC Seed replacement](https://bitquill.atlassian.net/browse/AD-506)
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Initial commit with support for Windows build without test.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Add support for compiling tests.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Correct GitHub workflow.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Correct GitHub workflow.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Correct GitHub workflow.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Disable building of test code.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Update .gitignore
    
    * adding mac build and workflow
    
    * temporarily disabling cppcheck
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Update for MacOS build script.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Re-enable cppcheck.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Disable cppcheck.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Add install of OpenSSL.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Set OPENSSL_ROOT_DIR for macOS.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Set OPENSSL_ROOT_DIR for macOS.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Set OPENSSL_ROOT_DIR for macOS.
    
    * [AD-504] Apache Ignite ODBC Seed replacement
    * Remove Window-specific method from MacOS/Linux code
    
    * [AD-504] Improve MacOS build.
    
    * [AD-504] Improve MacOS build docs.
    
    Co-authored-by: affonsov <affonsov@bitquilltech.com>

--- scripts/build_driver.ps1
+++ scripts/build_driver.ps1
@@ -4,12 +4,31 @@ $SRC_DIR = $args[2]
 $BUILD_DIR = $args[3]
 $INSTALL_DIR = $args[4]
 
+if ($WIN_ARCH -eq "x64") {
+    $VCPKG_TARGET_TRIPLET="x64-windows"
+} else {
+    $VCPKG_TARGET_TRIPLET="x86-windows"
+}
+
+if ($null -eq $env:VCPKG_ROOT) {
+    $env:VCPKG_ROOT = 'c:/vcpkg'
+}
+
+$CMAKE_TOOLCHAIN_FILE = -join($env:VCPKG_ROOT, "/scripts/buildsystems/vcpkg.cmake")
+
 cmake -S $SRC_DIR `
     -B $BUILD_DIR `
     -A $WIN_ARCH `
     -D CMAKE_BUILD_TYPE=$CONFIGURATION `
     -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR `
-    -D BUILD_WITH_TESTS=ON
+    -D WITH_TESTS=OFF `
+    -D WITH_CORE=OFF `
+    -D WITH_ODBC=ON `
+    -D WITH_ODBC_MSI=ON `
+    -D VCPKG_TARGET_TRIPLET=$VCPKG_TARGET_TRIPLET `
+    -D CMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE
 
-# # Build Project
-cmake --build $BUILD_DIR --config $CONFIGURATION --parallel 4 
+if ($?) {
+    # # Build Project
+    cmake --build $BUILD_DIR --target install --config $CONFIGURATION --parallel 4 
+}
