83f6e07ea63b3bc47e5e66a3a3802e6c41bc6c8e
affonsov <affonsov@bitquilltech.com>
2022-01-07 11:01:15 -0800

    enabling MacOS test and test coverage enabling MacOS debug build

--- build_mac_debug64.sh
+++ build_mac_debug64.sh
@@ -1,8 +1,5 @@
-# Build AWS SDK
-# $BITNESS=64
-
 mkdir cmake-build64
 cd cmake-build64
-cmake ../src -DCMAKE_BUILD_TYPE="Debug" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="OFF" -DWITH_CORE="OFF" -DWITH_ODBC="ON"
+cmake ../src -DCMAKE_BUILD_TYPE="Debug" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="ON" -DWITH_CORE="OFF" -DWITH_ODBC="ON"
 make ccov-all -j 4
 cd ..
