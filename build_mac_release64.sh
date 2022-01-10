ee3c9f7315104b129ab86fefd9d186f0b2092f1f
Bruce Irschick <brucei@bitquilltech.com>
2022-01-10 11:31:53 -0800

    [AD-513] Apache Ignite - Test Framework (Boost) (#5)
    ### Summary
    
    [AD-513] Apache Ignite - Test Framework (Boost)
    
    ### Description
    
    * Support for building test code on Windows
    
    ### Related Issue
    
    [[AD-513] Apache Ignite - Test Framework (Boost)](https://bitquill.atlassian.net/browse/AD-513)
    
    * [AD-513] Apache Ignite - Test Framework (Boost)
    * Support for building test code on Windows
    
    * [AD-513] Apache Ignite - Test Framework (Boost)
    * Remove unused/commented line from CMakeLists.txt
    
    * enabling MacOS test and test coverage
    enabling MacOS debug build
    
    * fixing mac-build workflow
    
    * fixing mac-build
    
    * removing extra quotes in mac-build
    
    * [AD-513] Remove unused environment variables, add tests.
    
    * [AD-513] Correct test path.
    
    * [AD-513] Add license header.
    
    * Adding TODO
    
    * adding extra line in the dummy test
    
    Co-authored-by: affonsov <affonsov@bitquilltech.com>

--- build_mac_release64.sh
+++ build_mac_release64.sh
@@ -1,8 +1,5 @@
-# Build AWS SDK
-# $BITNESS=64
-
 mkdir cmake-build64
 cd cmake-build64
-cmake ../src -DCMAKE_BUILD_TYPE="Release" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="OFF" -DWITH_CORE="OFF" -DWITH_ODBC="ON"
+cmake ../src -DCMAKE_BUILD_TYPE="Release" -DCODE_COVERAGE="OFF" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="ON" -DWITH_CORE="OFF" -DWITH_ODBC="ON"
 make -j 4
 cd ..
