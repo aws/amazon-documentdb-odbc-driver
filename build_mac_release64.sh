24faf1ab9ec6d3bd4a86c90e5f49a814bcd8d5d2
Bruce Irschick <brucei@bitquilltech.com>
2022-01-17 14:58:52 -0800

    [AD-506] * Make the build script exit on any error.

--- build_mac_release64.sh
+++ build_mac_release64.sh
@@ -4,6 +4,7 @@ BUILD_TYPE=Release
 PROJECT_DIR=$(pwd)
 DRIVER_BIN_DIR="$PROJECT_DIR/build/odbc/bin"
 
+set -e
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 cmake ../src -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCODE_COVERAGE="OFF" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="ON" -DWITH_CORE="OFF" -DWITH_ODBC="ON"
@@ -18,7 +19,7 @@ export DOCUMENTDB_HOME="$DRIVER_BIN_DIR"
 if [ ! -f "$JDBC_DRIVER_FULLPATH" ]; then
     mkdir "$PROJECT_DIR/libs"
     echo "Downloading version $JDBC_DRIVER_VERSION of JDBC driver..."
-    curl -v -o "$DRIVER_BIN_DIR/libs" -L https://github.com/aws/amazon-documentdb-jdbc-driver/releases/download/v$JDBC_DRIVER_VERSION/$JDBC_DRIVER_FILENAME
+    curl -o "$DRIVER_BIN_DIR/libs" -L https://github.com/aws/amazon-documentdb-jdbc-driver/releases/download/v$JDBC_DRIVER_VERSION/$JDBC_DRIVER_FILENAME
     echo "Download complete." 
 fi
 
