becf2d83a253cb5c45d277a2963a838869ba0b9c
andiem-bq <67925459+andiem-bq@users.noreply.github.com>
2022-02-03 09:29:23 -0800

    [AD-517] Adapt dsn config (#13)
    * [AD-517] Remove old properties + add new properties
    
    * [AD-517] Re-enable unit tests - WIP -  still need to cleanup
    
    * Add more unit tests
    
    * Remove extra file
    
    * Remove redundant declaration
    
    * [AD-517] * Change the C++ version to use to compile.
    
    * [AD-517] * Changed checks.yml so that it doesn't fail on code review errors.
    
    * Improvements from review
    
    * Updated comments
    
    * Removed includes no longer being used in configuration.h
    
    * Some formatting improvements
    
    * Improvements from code review + address some warnings
    
    * Change connection _test to use hostname key instead of address
    
    * Change connection_test to use databse key instead of schema key
    
    * Attempt to fix windows 32 build
    
    * Fix formatting
    
    * Improvements from code review
    
    * Improvement from code review - Remove new line between enum types
    
    * Change cmake version to latest for windows
    
    * Update cmake version to latest
    
    Co-authored-by: Bruce Irschick <brucei@bitquilltech.com>

--- src/odbc/src/query/batch_query.cpp
+++ src/odbc/src/query/batch_query.cpp
@@ -52,7 +52,7 @@ namespace ignite
                 if (executed)
                     Close();
 
-                int32_t maxPageSize = connection.GetConfiguration().GetPageSize();
+                int32_t maxPageSize = connection.GetConfiguration().GetDefaultFetchSize();
                 int32_t rowNum = params.GetParamSetSize();
                 SqlResult::Type res;
 
