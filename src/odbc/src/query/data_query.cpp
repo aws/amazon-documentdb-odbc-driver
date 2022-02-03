9ba1790ae03bd4b4f3beb9be58b28a30c05701a8
andiem-bq <67925459+andiem-bq@users.noreply.github.com>
2022-02-03 09:29:23 -0800

    [AD-517] Adapt dsn config (#13)
    * [AD-517] Remove old properties + add new properties
    
    * [AD-517] Re-enable unit tests - WIP -  still need to cleanup
    
    * Add more unit tests
    
    * Remove extra file
    
    * Remove redundant declaration
    
    * [AD-517] * Change the C++ version to use to compile.
    
    * [AD-517] * Changed checks.yml so that it doesn't fail on code review errors.
    
    * Improvements from review
    
    * Updated comments
    
    * Removed includes no longer being used in configuration.h
    
    * Some formatting improvements
    
    * Improvements from code review + address some warnings
    
    * Change connection _test to use hostname key instead of address
    
    * Change connection_test to use databse key instead of schema key
    
    * Attempt to fix windows 32 build
    
    * Fix formatting
    
    * Improvements from code review
    
    * Improvement from code review - Remove new line between enum types
    
    * Change cmake version to latest for windows
    
    * Update cmake version to latest
    
    Co-authored-by: Bruce Irschick <brucei@bitquilltech.com>

--- src/odbc/src/query/data_query.cpp
+++ src/odbc/src/query/data_query.cpp
@@ -193,7 +193,7 @@ namespace ignite
                 if (affected >= 0)
                     return affected;
 
-                return connection.GetConfiguration().GetPageSize();
+                return connection.GetConfiguration().GetDefaultFetchSize();
             }
 
             SqlResult::Type DataQuery::NextResultSet()
@@ -320,7 +320,7 @@ namespace ignite
             {
                 std::auto_ptr<ResultPage> resultPage(new ResultPage());
 
-                QueryFetchRequest req(cursor->GetQueryId(), connection.GetConfiguration().GetPageSize());
+                QueryFetchRequest req(cursor->GetQueryId(), connection.GetConfiguration().GetDefaultFetchSize());
                 QueryFetchResponse rsp(*resultPage);
 
                 try
@@ -361,7 +361,7 @@ namespace ignite
             {
                 std::auto_ptr<ResultPage> resultPage(new ResultPage());
 
-                QueryMoreResultsRequest req(cursor->GetQueryId(), connection.GetConfiguration().GetPageSize());
+                QueryMoreResultsRequest req(cursor->GetQueryId(), connection.GetConfiguration().GetDefaultFetchSize());
                 QueryMoreResultsResponse rsp(*resultPage);
 
                 try
