d837024e86f793eb62ba66f30b32f110401ef76a
affonsoBQ <67347924+affonsoBQ@users.noreply.github.com>
2022-02-16 10:12:33 -0800

    AD-516 Establish connection using MongoCPP (#12)
    Summary
    Establish connection using MongoCPP
    
    Description
    Integration with MongoCXX driver. Tracer code to estabilsh mongo connection
    
    Related Issue
    https://bitquill.atlassian.net/browse/AD-516

--- src/odbc/src/driverInstance.cpp
+++ src/odbc/src/driverInstance.cpp
@@ -15,28 +15,16 @@
  * limitations under the License.
  */
 
-#define BOOST_TEST_MODULE MyTest
-#include <boost/test/unit_test.hpp>
+#include "ignite/odbc/driverInstance.h"
+#include <mongocxx/instance.hpp>
 
-int add( int i, int j ) { return i+j; }
+DriverInstance& DriverInstance::getInstance() {
+    static DriverInstance driverInstance;
+    return driverInstance;
+}
 
-BOOST_AUTO_TEST_CASE( my_test )
-{
-    // seven ways to detect and report the same error:
-    BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
-
-    BOOST_REQUIRE( add( 2,2 ) == 4 );      // #2 throws on error
-
-    if( add( 2,2 ) != 4 )
-      BOOST_ERROR( "Ouch..." );            // #3 continues on error
-
-    if( add( 2,2 ) != 4 )
-      BOOST_FAIL( "Ouch..." );             // #4 throws on error
-
-    if( add( 2,2 ) != 4 ) throw "Ouch..."; // #5 throws on error
-
-    BOOST_CHECK_MESSAGE( add( 2,2 ) == 4,  // #6 continues on error
-                         "add(..) result: " << add( 2,2 ) );
-
-    BOOST_CHECK_EQUAL( add( 2,2 ), 4 );	  // #7 continues on error
+void DriverInstance::initialize() {
+    //Using current, current will get an existing instance or will create a new one.
+    //Maybe in future we will need to change this to use a custom logger.
+    mongocxx::instance::current();
 }
