7e7c06934a3d50552f34645e4031c50c99bbd8a7
Alina (Xi) Li <alinal@bitquilltech.com>
2022-02-25 10:54:08 -0800

    [AD-543] refactor copied ignite/binary/ files
    * refactor the files to be under odbc namespace

--- src/odbc/src/binary/binary_reader.cpp
+++ src/odbc/src/binary/binary_reader.cpp
@@ -15,174 +15,157 @@
  * limitations under the License.
  */
 
-#include "ignite/binary/binary_reader.h"
-#include "ignite/impl/binary/binary_reader_impl.h"
+#include "ignite/odbc/binary/binary_reader.h"
+#include "ignite/odbc/impl/binary/binary_reader_impl.h"
 
-using namespace ignite::impl::binary;
+using namespace ignite::odbc::impl::binary;
 
-namespace ignite
-{
-    namespace binary
-    {
-        BinaryReader::BinaryReader(BinaryReaderImpl* impl) : impl(impl)
-        {
-            // No-op.
-        }
-        
-        int8_t BinaryReader::ReadInt8(const char* fieldName)
-        {
-            return impl->ReadInt8(fieldName);
-        }
-
-        int32_t BinaryReader::ReadInt8Array(const char* fieldName, int8_t* res, int32_t len)
-        {
-            return impl->ReadInt8Array(fieldName, res, len);
-        }
-
-        bool BinaryReader::ReadBool(const char* fieldName)
-        {
-            return impl->ReadBool(fieldName);
-        }
-
-        int32_t BinaryReader::ReadBoolArray(const char* fieldName, bool* res, int32_t len)
-        {
-            return impl->ReadBoolArray(fieldName, res, len);
-        }
-
-        int16_t BinaryReader::ReadInt16(const char* fieldName)
-        {
-            return impl->ReadInt16(fieldName);
-        }
-
-        int32_t BinaryReader::ReadInt16Array(const char* fieldName, int16_t* res, int32_t len)
-        {
-            return impl->ReadInt16Array(fieldName, res, len);
-        }
-
-        uint16_t BinaryReader::ReadUInt16(const char* fieldName)
-        {
-            return impl->ReadUInt16(fieldName);
-        }
-
-        int32_t BinaryReader::ReadUInt16Array(const char* fieldName, uint16_t* res, int32_t len)
-        {
-            return impl->ReadUInt16Array(fieldName, res, len);
-        }
-
-        int32_t BinaryReader::ReadInt32(const char* fieldName)
-        {
-            return impl->ReadInt32(fieldName);
-        }
-
-        int32_t BinaryReader::ReadInt32Array(const char* fieldName, int32_t* res, int32_t len)
-        {
-            return impl->ReadInt32Array(fieldName, res, len);
-        }
-
-        int64_t BinaryReader::ReadInt64(const char* fieldName)
-        {
-            return impl->ReadInt64(fieldName);
-        }
-
-        int32_t BinaryReader::ReadInt64Array(const char* fieldName, int64_t* res, int32_t len)
-        {
-            return impl->ReadInt64Array(fieldName, res, len);
-        }
-
-        float BinaryReader::ReadFloat(const char* fieldName)
-        {
-            return impl->ReadFloat(fieldName);
-        }
-
-        int32_t BinaryReader::ReadFloatArray(const char* fieldName, float* res, int32_t len)
-        {
-            return impl->ReadFloatArray(fieldName, res, len);
-        }
-
-        double BinaryReader::ReadDouble(const char* fieldName)
-        {
-            return impl->ReadDouble(fieldName);
-        }
-
-        int32_t BinaryReader::ReadDoubleArray(const char* fieldName, double* res, int32_t len)
-        {
-            return impl->ReadDoubleArray(fieldName, res, len);
-        }
-
-        Guid BinaryReader::ReadGuid(const char* fieldName)
-        {
-            return impl->ReadGuid(fieldName);
-        }
-
-        int32_t BinaryReader::ReadGuidArray(const char* fieldName, Guid* res, int32_t len)
-        {
-            return impl->ReadGuidArray(fieldName, res, len);
-        }
-
-        Date BinaryReader::ReadDate(const char * fieldName)
-        {
-            return impl->ReadDate(fieldName);
-        }
-
-        int32_t BinaryReader::ReadDateArray(const char * fieldName, Date * res, const int32_t len)
-        {
-            return impl->ReadDateArray(fieldName, res, len);
-        }
-
-        Timestamp BinaryReader::ReadTimestamp(const char * fieldName)
-        {
-            return impl->ReadTimestamp(fieldName);
-        }
-
-        int32_t BinaryReader::ReadTimestampArray(const char * fieldName, Timestamp * res, const int32_t len)
-        {
-            return impl->ReadTimestampArray(fieldName, res, len);
-        }
-
-        Time BinaryReader::ReadTime(const char* fieldName)
-        {
-            return impl->ReadTime(fieldName);
-        }
-
-        int32_t BinaryReader::ReadTimeArray(const char* fieldName, Time* res, const int32_t len)
-        {
-            return impl->ReadTimeArray(fieldName, res, len);
-        }
-
-        int32_t BinaryReader::ReadString(const char* fieldName, char* res, int32_t len)
-        {
-            return impl->ReadString(fieldName, res, len);
-        }
-
-        BinaryStringArrayReader BinaryReader::ReadStringArray(const char* fieldName)
-        {
-            int32_t size;
-
-            int32_t id = impl->ReadStringArray(fieldName, &size);
-
-            return BinaryStringArrayReader(impl, id, size);
-        }
-
-        BinaryEnumEntry BinaryReader::ReadBinaryEnum(const char* fieldName)
-        {
-            return impl->ReadBinaryEnum(fieldName);
-        }
-
-        CollectionType::Type BinaryReader::ReadCollectionType(const char* fieldName)
-        {
-            return impl->ReadCollectionType(fieldName);
-        }
-
-        int32_t BinaryReader::ReadCollectionSize(const char* fieldName)
-        {
-            return impl->ReadCollectionSize(fieldName);
-        }
-
-        BinaryRawReader BinaryReader::RawReader()
-        {
-            impl->SetRawMode();
-
-            return BinaryRawReader(impl);
-        }
-    }
+namespace ignite {
+namespace odbc {
+namespace binary {
+BinaryReader::BinaryReader(BinaryReaderImpl* impl) : impl(impl) {
+  // No-op.
 }
+
+int8_t BinaryReader::ReadInt8(const char* fieldName) {
+  return impl->ReadInt8(fieldName);
+}
+
+int32_t BinaryReader::ReadInt8Array(const char* fieldName, int8_t* res,
+                                    int32_t len) {
+  return impl->ReadInt8Array(fieldName, res, len);
+}
+
+bool BinaryReader::ReadBool(const char* fieldName) {
+  return impl->ReadBool(fieldName);
+}
+
+int32_t BinaryReader::ReadBoolArray(const char* fieldName, bool* res,
+                                    int32_t len) {
+  return impl->ReadBoolArray(fieldName, res, len);
+}
+
+int16_t BinaryReader::ReadInt16(const char* fieldName) {
+  return impl->ReadInt16(fieldName);
+}
+
+int32_t BinaryReader::ReadInt16Array(const char* fieldName, int16_t* res,
+                                     int32_t len) {
+  return impl->ReadInt16Array(fieldName, res, len);
+}
+
+uint16_t BinaryReader::ReadUInt16(const char* fieldName) {
+  return impl->ReadUInt16(fieldName);
+}
+
+int32_t BinaryReader::ReadUInt16Array(const char* fieldName, uint16_t* res,
+                                      int32_t len) {
+  return impl->ReadUInt16Array(fieldName, res, len);
+}
+
+int32_t BinaryReader::ReadInt32(const char* fieldName) {
+  return impl->ReadInt32(fieldName);
+}
+
+int32_t BinaryReader::ReadInt32Array(const char* fieldName, int32_t* res,
+                                     int32_t len) {
+  return impl->ReadInt32Array(fieldName, res, len);
+}
+
+int64_t BinaryReader::ReadInt64(const char* fieldName) {
+  return impl->ReadInt64(fieldName);
+}
+
+int32_t BinaryReader::ReadInt64Array(const char* fieldName, int64_t* res,
+                                     int32_t len) {
+  return impl->ReadInt64Array(fieldName, res, len);
+}
+
+float BinaryReader::ReadFloat(const char* fieldName) {
+  return impl->ReadFloat(fieldName);
+}
+
+int32_t BinaryReader::ReadFloatArray(const char* fieldName, float* res,
+                                     int32_t len) {
+  return impl->ReadFloatArray(fieldName, res, len);
+}
+
+double BinaryReader::ReadDouble(const char* fieldName) {
+  return impl->ReadDouble(fieldName);
+}
+
+int32_t BinaryReader::ReadDoubleArray(const char* fieldName, double* res,
+                                      int32_t len) {
+  return impl->ReadDoubleArray(fieldName, res, len);
+}
+
+Guid BinaryReader::ReadGuid(const char* fieldName) {
+  return impl->ReadGuid(fieldName);
+}
+
+int32_t BinaryReader::ReadGuidArray(const char* fieldName, Guid* res,
+                                    int32_t len) {
+  return impl->ReadGuidArray(fieldName, res, len);
+}
+
+Date BinaryReader::ReadDate(const char* fieldName) {
+  return impl->ReadDate(fieldName);
+}
+
+int32_t BinaryReader::ReadDateArray(const char* fieldName, Date* res,
+                                    const int32_t len) {
+  return impl->ReadDateArray(fieldName, res, len);
+}
+
+Timestamp BinaryReader::ReadTimestamp(const char* fieldName) {
+  return impl->ReadTimestamp(fieldName);
+}
+
+int32_t BinaryReader::ReadTimestampArray(const char* fieldName, Timestamp* res,
+                                         const int32_t len) {
+  return impl->ReadTimestampArray(fieldName, res, len);
+}
+
+Time BinaryReader::ReadTime(const char* fieldName) {
+  return impl->ReadTime(fieldName);
+}
+
+int32_t BinaryReader::ReadTimeArray(const char* fieldName, Time* res,
+                                    const int32_t len) {
+  return impl->ReadTimeArray(fieldName, res, len);
+}
+
+int32_t BinaryReader::ReadString(const char* fieldName, char* res,
+                                 int32_t len) {
+  return impl->ReadString(fieldName, res, len);
+}
+
+BinaryStringArrayReader BinaryReader::ReadStringArray(const char* fieldName) {
+  int32_t size;
+
+  int32_t id = impl->ReadStringArray(fieldName, &size);
+
+  return BinaryStringArrayReader(impl, id, size);
+}
+
+BinaryEnumEntry BinaryReader::ReadBinaryEnum(const char* fieldName) {
+  return impl->ReadBinaryEnum(fieldName);
+}
+
+CollectionType::Type BinaryReader::ReadCollectionType(const char* fieldName) {
+  return impl->ReadCollectionType(fieldName);
+}
+
+int32_t BinaryReader::ReadCollectionSize(const char* fieldName) {
+  return impl->ReadCollectionSize(fieldName);
+}
+
+BinaryRawReader BinaryReader::RawReader() {
+  impl->SetRawMode();
+
+  return BinaryRawReader(impl);
+}
+}  // namespace binary
+}  // namespace odbc
+}  // namespace ignite
+
--- src/odbc/src/binary/binary_writer.cpp
+++ src/odbc/src/binary/binary_writer.cpp
@@ -15,175 +15,160 @@
  * limitations under the License.
  */
 
-#include "ignite/impl/binary/binary_writer_impl.h"
-#include "ignite/binary/binary_writer.h"
+#include "ignite/odbc/impl/binary/binary_writer_impl.h"
+#include "ignite/odbc/binary/binary_writer.h"
 
-using namespace ignite::impl::binary;
+using namespace ignite::odbc::impl::binary;
 
-namespace ignite
-{
-    namespace binary
-    {
-        BinaryWriter::BinaryWriter(BinaryWriterImpl* impl) : impl(impl)
-        {
-            // No-op.
-        }
-
-        void BinaryWriter::WriteInt8(const char* fieldName, int8_t val)
-        {
-            impl->WriteInt8(fieldName, val);
-        }
-
-        void BinaryWriter::WriteInt8Array(const char* fieldName, const int8_t* val, int32_t len)
-        {
-            impl->WriteInt8Array(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteBool(const char* fieldName, bool val)
-        {
-            impl->WriteBool(fieldName, val);
-        }
-
-        void BinaryWriter::WriteBoolArray(const char* fieldName, const bool* val, int32_t len)
-        {
-            impl->WriteBoolArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteInt16(const char* fieldName, int16_t val)
-        {
-            impl->WriteInt16(fieldName, val);
-        }
-
-        void BinaryWriter::WriteInt16Array(const char* fieldName, const int16_t* val, int32_t len)
-        {
-            impl->WriteInt16Array(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteUInt16(const char* fieldName, uint16_t val)
-        {
-            impl->WriteUInt16(fieldName, val);
-        }
-
-        void BinaryWriter::WriteUInt16Array(const char* fieldName, const uint16_t* val, int32_t len)
-        {
-            impl->WriteUInt16Array(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteInt32(const char* fieldName, int32_t val)
-        {
-            impl->WriteInt32(fieldName, val);
-        }
-
-        void BinaryWriter::WriteInt32Array(const char* fieldName, const int32_t* val, int32_t len)
-        {
-            impl->WriteInt32Array(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteInt64(const char* fieldName, const int64_t val)
-        {
-            impl->WriteInt64(fieldName, val);
-        }
-
-        void BinaryWriter::WriteInt64Array(const char* fieldName, const int64_t* val, int32_t len)
-        {
-            impl->WriteInt64Array(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteFloat(const char* fieldName, float val)
-        {
-            impl->WriteFloat(fieldName, val);
-        }
-
-        void BinaryWriter::WriteFloatArray(const char* fieldName, const float* val, int32_t len)
-        {
-            impl->WriteFloatArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteDouble(const char* fieldName, double val)
-        {
-            impl->WriteDouble(fieldName, val);
-        }
-
-        void BinaryWriter::WriteDoubleArray(const char* fieldName, const double* val, int32_t len)
-        {
-            impl->WriteDoubleArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteGuid(const char* fieldName, const Guid& val)
-        {
-            impl->WriteGuid(fieldName, val);
-        }
-
-        void BinaryWriter::WriteGuidArray(const char* fieldName, const Guid* val, const int32_t len)
-        {
-            impl->WriteGuidArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteDate(const char * fieldName, const Date & val)
-        {
-            impl->WriteDate(fieldName, val);
-        }
-
-        void BinaryWriter::WriteDateArray(const char * fieldName, const Date * val, const int32_t len)
-        {
-            impl->WriteDateArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteTimestamp(const char * fieldName, const Timestamp & val)
-        {
-            impl->WriteTimestamp(fieldName, val);
-        }
-
-        void BinaryWriter::WriteTimestampArray(const char * fieldName, const Timestamp * val, const int32_t len)
-        {
-            impl->WriteTimestampArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteTime(const char* fieldName, const Time& val)
-        {
-            impl->WriteTime(fieldName, val);
-        }
-
-        void BinaryWriter::WriteTimeArray(const char* fieldName, const Time* val, const int32_t len)
-        {
-            impl->WriteTimeArray(fieldName, val, len);
-        }
-
-        void BinaryWriter::WriteString(const char* fieldName, const char* val)
-        {
-            if (val)
-                WriteString(fieldName, val, static_cast<int32_t>(strlen(val)));
-            else
-                WriteNull(fieldName);
-        }
-
-        void BinaryWriter::WriteString(const char* fieldName, const char* val, int32_t len)
-        {
-            impl->WriteString(fieldName, val, len);
-        }
-
-        BinaryStringArrayWriter BinaryWriter::WriteStringArray(const char* fieldName)
-        {
-            int32_t id = impl->WriteStringArray(fieldName);
-
-            return BinaryStringArrayWriter(impl, id);
-        }
-
-        void BinaryWriter::WriteBinaryEnum(const char* fieldName, BinaryEnumEntry entry)
-        {
-            impl->WriteBinaryEnum(fieldName, entry);
-        }
-
-        void BinaryWriter::WriteNull(const char* fieldName)
-        {
-            impl->WriteNull(fieldName);
-        }
-
-        BinaryRawWriter BinaryWriter::RawWriter()
-        {
-            impl->SetRawMode();
-
-            return BinaryRawWriter(impl);
-        }
-    }
+namespace ignite {
+namespace odbc {
+namespace binary {
+BinaryWriter::BinaryWriter(BinaryWriterImpl* impl) : impl(impl) {
+  // No-op.
 }
+
+void BinaryWriter::WriteInt8(const char* fieldName, int8_t val) {
+  impl->WriteInt8(fieldName, val);
+}
+
+void BinaryWriter::WriteInt8Array(const char* fieldName, const int8_t* val,
+                                  int32_t len) {
+  impl->WriteInt8Array(fieldName, val, len);
+}
+
+void BinaryWriter::WriteBool(const char* fieldName, bool val) {
+  impl->WriteBool(fieldName, val);
+}
+
+void BinaryWriter::WriteBoolArray(const char* fieldName, const bool* val,
+                                  int32_t len) {
+  impl->WriteBoolArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteInt16(const char* fieldName, int16_t val) {
+  impl->WriteInt16(fieldName, val);
+}
+
+void BinaryWriter::WriteInt16Array(const char* fieldName, const int16_t* val,
+                                   int32_t len) {
+  impl->WriteInt16Array(fieldName, val, len);
+}
+
+void BinaryWriter::WriteUInt16(const char* fieldName, uint16_t val) {
+  impl->WriteUInt16(fieldName, val);
+}
+
+void BinaryWriter::WriteUInt16Array(const char* fieldName, const uint16_t* val,
+                                    int32_t len) {
+  impl->WriteUInt16Array(fieldName, val, len);
+}
+
+void BinaryWriter::WriteInt32(const char* fieldName, int32_t val) {
+  impl->WriteInt32(fieldName, val);
+}
+
+void BinaryWriter::WriteInt32Array(const char* fieldName, const int32_t* val,
+                                   int32_t len) {
+  impl->WriteInt32Array(fieldName, val, len);
+}
+
+void BinaryWriter::WriteInt64(const char* fieldName, const int64_t val) {
+  impl->WriteInt64(fieldName, val);
+}
+
+void BinaryWriter::WriteInt64Array(const char* fieldName, const int64_t* val,
+                                   int32_t len) {
+  impl->WriteInt64Array(fieldName, val, len);
+}
+
+void BinaryWriter::WriteFloat(const char* fieldName, float val) {
+  impl->WriteFloat(fieldName, val);
+}
+
+void BinaryWriter::WriteFloatArray(const char* fieldName, const float* val,
+                                   int32_t len) {
+  impl->WriteFloatArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteDouble(const char* fieldName, double val) {
+  impl->WriteDouble(fieldName, val);
+}
+
+void BinaryWriter::WriteDoubleArray(const char* fieldName, const double* val,
+                                    int32_t len) {
+  impl->WriteDoubleArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteGuid(const char* fieldName, const Guid& val) {
+  impl->WriteGuid(fieldName, val);
+}
+
+void BinaryWriter::WriteGuidArray(const char* fieldName, const Guid* val,
+                                  const int32_t len) {
+  impl->WriteGuidArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteDate(const char* fieldName, const Date& val) {
+  impl->WriteDate(fieldName, val);
+}
+
+void BinaryWriter::WriteDateArray(const char* fieldName, const Date* val,
+                                  const int32_t len) {
+  impl->WriteDateArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteTimestamp(const char* fieldName, const Timestamp& val) {
+  impl->WriteTimestamp(fieldName, val);
+}
+
+void BinaryWriter::WriteTimestampArray(const char* fieldName,
+                                       const Timestamp* val,
+                                       const int32_t len) {
+  impl->WriteTimestampArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteTime(const char* fieldName, const Time& val) {
+  impl->WriteTime(fieldName, val);
+}
+
+void BinaryWriter::WriteTimeArray(const char* fieldName, const Time* val,
+                                  const int32_t len) {
+  impl->WriteTimeArray(fieldName, val, len);
+}
+
+void BinaryWriter::WriteString(const char* fieldName, const char* val) {
+  if (val)
+    WriteString(fieldName, val, static_cast< int32_t >(strlen(val)));
+  else
+    WriteNull(fieldName);
+}
+
+void BinaryWriter::WriteString(const char* fieldName, const char* val,
+                               int32_t len) {
+  impl->WriteString(fieldName, val, len);
+}
+
+BinaryStringArrayWriter BinaryWriter::WriteStringArray(const char* fieldName) {
+  int32_t id = impl->WriteStringArray(fieldName);
+
+  return BinaryStringArrayWriter(impl, id);
+}
+
+void BinaryWriter::WriteBinaryEnum(const char* fieldName,
+                                   BinaryEnumEntry entry) {
+  impl->WriteBinaryEnum(fieldName, entry);
+}
+
+void BinaryWriter::WriteNull(const char* fieldName) {
+  impl->WriteNull(fieldName);
+}
+
+BinaryRawWriter BinaryWriter::RawWriter() {
+  impl->SetRawMode();
+
+  return BinaryRawWriter(impl);
+}
+}  // namespace binary
+}  // namespace odbc
+}  // namespace ignite
+
