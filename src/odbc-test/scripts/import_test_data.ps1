291d8306738c59e41c5aa6d7d65b4e22f765a631
Bruce Irschick <brucei@bitquilltech.com>
2022-03-03 10:51:51 -0800

    [AD-607] [ODBC] * Work on MacOS MongoDB setup

--- src/odbc-test/scripts/import_test_data.ps1
+++ src/odbc-test/scripts/import_test_data.ps1
@@ -1,40 +1,32 @@
 ##################################################
 # Assumptions
-# 1. Docker container 'mongo' is running and instance of mongo server and listening on port 27017
-# 2. Mongo started with the environment variables MONGO_INITDB_ROOT_USERNAME and MONGO_INITDB_ROOT_PASSWORD
+# 1. MongoDB server is running and listening on port 27017
+# 2. Environment variables DOC_DB_USER_NAME and DOC_DB_PASSWORD already defined
 ##################################################
 
 $TEST_INPUT_FOLDER = Join-Path -Path (Get-Item $PSCommandPath).DirectoryName  -ChildPath "..\input"
-$CONTAINER_NAME  = if ( $env:CONTAINER_NAME ) { $env:CONTAINER_NAME } else { "mongo" }
 $DATABASE_NAME   = "odbc-test"
-$CONTAINER_INPUT_FOLDER = "/home/odbc-test"
 
-# Copy test files to docker and drop database.
-docker exec -i $CONTAINER_NAME bash -c "rm -fr $CONTAINER_INPUT_FOLDER"
-docker exec -i $CONTAINER_NAME bash -c "mkdir -p $CONTAINER_INPUT_FOLDER"
-docker cp $TEST_INPUT_FOLDER "$($CONTAINER_NAME):$($CONTAINER_INPUT_FOLDER)"
 # Clear the database so we don't have any existing data
-docker exec -i "$($CONTAINER_NAME)" `
-    mongosh --quiet -u="$($env:MONGO_INITDB_ROOT_USERNAME)" -p="$($env:MONGO_INITDB_ROOT_PASSWORD)" --authenticationDatabase=admin `
+mongosh --quiet -u="$($env:DOC_DB_USER_NAME)" -p="$($env:DOC_DB_PASSWORD)" --authenticationDatabase=admin `
         --eval "db.dropDatabase()" "$($DATABASE_NAME)"
 
 # Load each test input file
 Get-ChildItem -Path $TEST_INPUT_FOLDER -File -Filter *.json |
 Foreach-Object {
     # Collection will be named the same as the input file base name
-	$COLLECTION_NAME = $_.BaseName
-	$TEST_FILE_NAME  = $_.Name
+    $COLLECTION_NAME = $_.BaseName
+    $TEST_FILE_NAME  = $_.Name
 
     # Log what input we're about to import
-	Write-Output "docker exec -i $($CONTAINER_NAME) `
-        mongoimport -u=$($env:MONGO_INITDB_ROOT_USERNAME) -p=... --authenticationDatabase=admin `
+    Write-Output `
+        "mongoimport --quiet -u=$($env:DOC_DB_USER_NAME) -p=... --authenticationDatabase=admin `
             -d=$($DATABASE_NAME) -c=$($COLLECTION_NAME) `
-            --file=""$($CONTAINER_INPUT_FOLDER)/input/$($TEST_FILE_NAME)"""
+            --file=""$($TEST_INPUT_FOLDER)\$($TEST_FILE_NAME)"""
     # Import the test input
-	docker exec -i "$($CONTAINER_NAME)" `
-        mongoimport --quiet -u="$($env:MONGO_INITDB_ROOT_USERNAME)" -p="$($env:MONGO_INITDB_ROOT_PASSWORD)" --authenticationDatabase=admin `
-            -d="$($DATABASE_NAME)" -c="$($COLLECTION_NAME)" `
-            --file="""$($CONTAINER_INPUT_FOLDER)/input/$($TEST_FILE_NAME)"""
+    mongoimport --quiet -u="$($env:DOC_DB_USER_NAME)" -p="$($env:DOC_DB_PASSWORD)" --authenticationDatabase=admin `
+        -d="$($DATABASE_NAME)" -c="$($COLLECTION_NAME)" `
+        --file="""$($TEST_INPUT_FOLDER)\$($TEST_FILE_NAME)"""
     if (!$?) {
         # If error detected stop the rest of the processing and exit
         exit 1
--- src/odbc-test/scripts/import_test_data.sh
+++ src/odbc-test/scripts/import_test_data.sh
@@ -3,23 +3,16 @@
 ##################################################
 # Assumptions
 # 1. Docker container 'mongo' is running and instance of mongo server and listening on port 27017
-# 2. Mongo started with the environment variables MONGO_INITDB_ROOT_USERNAME and MONGO_INITDB_ROOT_PASSWORD
+# 2. Mongo started with the environment variables DOC_DB_USER_NAME and DOC_DB_PASSWORD
 ##################################################
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_INPUT_FOLDER="${SCRIPT_DIR}/../input"
-CONTAINER_NAME=${CONTAINER_NAME:-mongo}
 DATABASE_NAME="odbc-test"
-CONTAINER_INPUT_FOLDER="/home/odbc-test"
 
-# Copy test files to docker and drop database.
-docker exec ${CONTAINER_NAME} bash -c "rm -fr ${CONTAINER_INPUT_FOLDER}"
-docker exec ${CONTAINER_NAME} bash -c "mkdir -p ${CONTAINER_INPUT_FOLDER}"
-docker cp $TEST_INPUT_FOLDER "${CONTAINER_NAME}:${CONTAINER_INPUT_FOLDER}"
 # Clear the database so we don't have any existing data
-docker exec "${CONTAINER_NAME}" \
-    mongosh --quiet -u="${MONGO_INITDB_ROOT_USERNAME}" -p="${MONGO_INITDB_ROOT_PASSWORD}" --authenticationDatabase=admin \
-        --eval "db.dropDatabase()" "${DATABASE_NAME}"
+mongosh --quiet -u="${DOC_DB_USER_NAME}" -p="${DOC_DB_PASSWORD}" --authenticationDatabase=admin \
+    --eval "db.dropDatabase()" "${DATABASE_NAME}"
 
 # Load each test input file
 for FILENAME in ${TEST_INPUT_FOLDER}/*.json
@@ -28,10 +21,9 @@ do
     COLLECTION_NAME="$(basename -- ${FILENAME%.json})"
     TEST_FILE_NAME="$(basename -- ${FILENAME})"
 
-    docker exec "${CONTAINER_NAME}" \
-        mongoimport --quiet -u="${MONGO_INITDB_ROOT_USERNAME}" -p="${MONGO_INITDB_ROOT_PASSWORD}" --authenticationDatabase=admin \
-            -d="${DATABASE_NAME}" -c="${COLLECTION_NAME}" \
-            --file="""${CONTAINER_INPUT_FOLDER}/input/${TEST_FILE_NAME}"""
+    mongoimport --quiet -u="${DOC_DB_USER_NAME}" -p="${DOC_DB_PASSWORD}" --authenticationDatabase=admin \
+        -d="${DATABASE_NAME}" -c="${COLLECTION_NAME}" \
+        --file="""${TEST_INPUT_FOLDER}/${TEST_FILE_NAME}"""
     if [ $? -ne 0 ]
     then
         exit 1
