5f5700659b283f52c557a32051a60cfa50c6f1ec
Alina (Xi) Li <alinal@bitquilltech.com>
2022-03-03 16:46:12 -0800

    [AD-608] bugfix include

--- src/odbc/include/ignite/odbc/meta/column_meta.h
+++ src/odbc/include/ignite/odbc/meta/column_meta.h
@@ -23,7 +23,7 @@
 #include <string>
 
 #include <boost/optional.hpp>
-#include <boost/optional/optional_io.hpp >
+#include <boost/optional/optional_io.hpp>
 
 #include "ignite/impl/binary/binary_reader_impl.h"
 #include "ignite/odbc/common_types.h"
