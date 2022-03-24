adc01855e985801c01cfc8c780424ef3f752898f
Alina (Xi) Li <alinal@bitquilltech.com>
2022-03-24 16:39:18 -0700

    [AD-591] remove old Ignite test on SQLNumResultCols

--- src/odbc-test/src/api_robustness_test.cpp
+++ src/odbc-test/src/api_robustness_test.cpp
@@ -368,37 +368,6 @@ BOOST_AUTO_TEST_CASE(TestSQLNumResultCols) {
   SQLNumResultCols(stmt, 0);
 }
 
-BOOST_AUTO_TEST_CASE(TestIgniteSQLNumResultCols, *disabled()) {
-  // There are no checks because we do not really care what is the result of
-  // these calls as long as they do not cause segmentation fault.
-
-  for (int i = 0; i < 100; ++i) {
-    TestType obj;
-
-    obj.strField = LexicalCast< std::string >(i);
-
-    testCache.Put(i, obj);
-  }
-
-  Connect("DRIVER={Apache Ignite};address=127.0.0.1:11110;schema=cache");
-
-  SQLCHAR sql[] = "SELECT strField FROM TestType";
-
-  SQLRETURN ret = SQLExecDirect(stmt, sql, sizeof(sql));
-
-  ODBC_FAIL_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
-
-  SQLSMALLINT columnCount;
-
-  // Everything is ok.
-  ret = SQLNumResultCols(stmt, &columnCount);
-
-  ODBC_FAIL_ON_ERROR(ret, SQL_HANDLE_STMT, stmt);
-
-  // Column count is null.
-  SQLNumResultCols(stmt, 0);
-}
-
 BOOST_AUTO_TEST_CASE(TestSQLTables, *disabled()) {
   // There are no checks because we do not really care what is the result of
   // these calls as long as they do not cause segmentation fault.
