5003fae5237e5e918b449ccbd5510df5f0c63af9
affonsov <affonsov@bitquilltech.com>
2022-03-25 16:01:04 -0700

    add check env variables are set fixing build issues

---
+++ scripts/env_variables_check.sh
@@ -0,0 +1,43 @@
+CHECK=1
+if [[ -z "${DOC_DB_USER_NAME}" ]]; then
+  echo "DOC_DB_USER_NAME environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${DOC_DB_PASSWORD}" ]]; then
+  echo "DOC_DB_PASSWORD environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${DOC_DB_USER}" ]]; then
+  echo "DOC_DB_USER environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${DOC_DB_USER}" ]]; then
+  echo "DOC_DB_USER environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${DOC_DB_LOCAL_PORT}" ]]; then
+  echo "DOC_DB_LOCAL_PORT environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${DOC_DB_PRIV_KEY_FILE}" ]]; then
+  echo "DOC_DB_PRIV_KEY_FILE environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${JAVA_HOME}" ]]; then
+  echo "DOC_DB_USER_NAME environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${DOCUMENTDB_HOME}" ]]; then
+  echo "DOCUMENTDB_HOME environment variable is not set"
+  CHECK=0
+fi
+if [[ -z "${ODBC_LIB_PATH}" ]]; then
+  echo "ODBC_LIB_PATH environment variable is not set"
+  CHECK=0
+fi
+
+if [[ -z "${CHECK}" ]]; then
+  echo "Missing envrionment variables please set them accordingly."
+else
+  echo "Environment variables are all set"
+fi
