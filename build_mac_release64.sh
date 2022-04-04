2e5788f2a2943bc2d55373fd49ac6f2baf453725
Alina (Xi) Li <96995091+alinaliBQ@users.noreply.github.com>
2022-04-04 09:51:20 -0700

    [AD-673] bump JDBC version to 1.2.1 (#61)

--- build_mac_release64.sh
+++ build_mac_release64.sh
@@ -10,7 +10,7 @@ make -j 4
 
 # Download the DocumentDB JDBC Driver
 if [ -z "$JDBC_DRIVER_VERSION" ]; then 
-    JDBC_DRIVER_VERSION="1.2.0"
+    JDBC_DRIVER_VERSION="1.2.1"
 fi
 JDBC_DRIVER_FILENAME="documentdb-jdbc-$JDBC_DRIVER_VERSION-all.jar"
 JDBC_DRIVER_FULLPATH="$DRIVER_BIN_DIR/libs/$JDBC_DRIVER_FILENAME"
