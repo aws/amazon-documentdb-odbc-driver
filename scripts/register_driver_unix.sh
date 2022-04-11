1141f51ef2cb0bca8b907256d96cd570e713dc11
affonsoBQ <67347924+affonsoBQ@users.noreply.github.com>
2022-04-11 13:11:24 -0700

    AD-660 enable linux build (#54)
    * enabling linux build

--- scripts/register_driver_unix.sh
+++ scripts/register_driver_unix.sh
@@ -10,7 +10,12 @@ SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 PROJECT_DIR="$SCRIPT_DIR/.."
 ODBC_LIB_PATH="$PROJECT_DIR/build/odbc/lib"
-ODBC_LIB_FILENAME="$ODBC_LIB_PATH/libignite-odbc.dylib"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  ODBC_LIB_FILENAME="$ODBC_LIB_PATH/libignite-odbc.so"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  ODBC_LIB_FILENAME="$ODBC_LIB_PATH/libignite-odbc.dylib"
+fi
+
 
 if [ ! -f "$ODBC_LIB_FILENAME" ]
 then
@@ -24,4 +29,5 @@ echo "Driver=$ODBC_LIB_FILENAME" >> "$ODBC_LIB_PATH/ignite-odbc-install.ini"
 echo "Setup=$ODBC_LIB_FILENAME"  >> "$ODBC_LIB_PATH/ignite-odbc-install.ini"
 echo "DriverODBCVer=03.00"       >> "$ODBC_LIB_PATH/ignite-odbc-install.ini"
 echo "FileUsage=0"               >> "$ODBC_LIB_PATH/ignite-odbc-install.ini"
-odbcinst -i -d                   -f "$ODBC_LIB_PATH/ignite-odbc-install.ini"
+
+odbcinst -i -d -f "$ODBC_LIB_PATH/ignite-odbc-install.ini"
