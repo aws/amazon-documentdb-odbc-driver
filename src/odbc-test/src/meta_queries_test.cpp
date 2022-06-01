6f4d922a48f56b0cf32ef5e4e55ca33bdb97184f
Alina (Xi) Li <alinal@bitquilltech.com>
2022-05-31 17:28:35 -0700

    [AD-621] test for SQL_DESC_LITERAL_PREFIX

--- src/odbc-test/src/meta_queries_test.cpp
+++ src/odbc-test/src/meta_queries_test.cpp
@@ -647,6 +647,72 @@ BOOST_AUTO_TEST_CASE(TestColAttributeDescCaseSensitive) {
   BOOST_CHECK_EQUAL(intVal, SQL_FALSE);
 }
 
+BOOST_AUTO_TEST_CASE(TestColAttributeDescLiteralPrefix) {
+  std::string dsnConnectionString;
+  std::string databaseName("odbc-test");
+  CreateDsnConnectionStringForLocalServer(dsnConnectionString, databaseName);
+
+  Connect(dsnConnectionString);
+
+  // test that empty string is returned for non-char and non-binary type
+  SQLCHAR req[] = "select fieldDouble from meta_queries_test_001";
+  SQLExecDirect(stmt, req, SQL_NTS);
+
+  SQLLEN intVal;
+  SQLCHAR strBuf[1024];
+  SQLSMALLINT strLen;
+
+  SQLRETURN ret = SQLColAttribute(stmt, 1, SQL_DESC_LITERAL_PREFIX, strBuf,
+                                  sizeof(strBuf), &strLen, &intVal);
+
+  if (!SQL_SUCCEEDED(ret))
+    BOOST_FAIL(GetOdbcErrorMessage(SQL_HANDLE_STMT, stmt));
+
+  // convert SQLCHAR[] strBuf to to std::string buf
+  std::stringstream bufStream;
+  bufStream << strBuf;
+  std::string buf;
+  bufStream >> buf;
+
+  BOOST_CHECK("" == buf);
+
+  // test that "'" is returned for *CHAR type
+  SQLCHAR req2[] = "select fieldString from meta_queries_test_002";
+  SQLExecDirect(stmt, req2, SQL_NTS);
+
+  ret = SQLColAttribute(stmt, 1, SQL_DESC_LITERAL_PREFIX, strBuf,
+                                  sizeof(strBuf), &strLen, &intVal);
+
+  if (!SQL_SUCCEEDED(ret))
+    BOOST_FAIL(GetOdbcErrorMessage(SQL_HANDLE_STMT, stmt));
+
+  // convert SQLCHAR[] strBuf to to std::string buf
+  bufStream.clear();
+  buf.clear();
+  bufStream << strBuf;
+  bufStream >> buf;
+
+  BOOST_CHECK("'" == buf);
+
+  // test that "0x" is returned for *CHAR type
+  SQLCHAR req3[] = "select fieldBinary from meta_queries_test_002";
+  SQLExecDirect(stmt, req3, SQL_NTS);
+
+  ret = SQLColAttribute(stmt, 1, SQL_DESC_LITERAL_PREFIX, strBuf,
+                        sizeof(strBuf), &strLen, &intVal);
+
+  if (!SQL_SUCCEEDED(ret))
+    BOOST_FAIL(GetOdbcErrorMessage(SQL_HANDLE_STMT, stmt));
+
+  // convert SQLCHAR[] strBuf to to std::string buf
+  bufStream.clear();
+  buf.clear();
+  bufStream << strBuf;
+  bufStream >> buf;
+
+  BOOST_CHECK("0x" == buf);
+}
+
 BOOST_AUTO_TEST_CASE(TestColAttributesColumnScale, *disabled()) {
   Connect("DRIVER={Apache Ignite};ADDRESS=127.0.0.1:11110;SCHEMA=cache");
 
