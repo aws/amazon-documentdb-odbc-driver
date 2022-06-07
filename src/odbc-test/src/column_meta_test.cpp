e4dbc1880ecdfc6b148ba96a8468d0009dc93bec
alinaliBQ <alinal@bitquilltech.com>
2022-06-07 13:53:23 -0700

    [AD-621] add more checks for TestGetAttribute

--- src/odbc-test/src/column_meta_test.cpp
+++ src/odbc-test/src/column_meta_test.cpp
@@ -21,28 +21,29 @@
 #include <sqlext.h>
 
 #include <boost/test/unit_test.hpp>
+#include <utility>
 
+#include "ignite/odbc/impl/binary/binary_common.h"
 #include "ignite/odbc/meta/column_meta.h"
 #include "ignite/odbc/type_traits.h"
-#include "ignite/odbc/impl/binary/binary_common.h"
-
 #include "odbc_test_suite.h"
 
+using namespace ignite::odbc::impl::binary;
 using ignite::odbc::OdbcTestSuite;
 using ignite::odbc::meta::ColumnMeta;
 using ignite::odbc::meta::Nullability;
 
 BOOST_AUTO_TEST_CASE(TestGetAttribute) {
   // Only SQL_DESC_* fields are tested in this test.
-  // This is because those are the fields that would be passed to 
-  // SQLColAttribute function. 
+  // This is because those are the fields that would be passed to
+  // SQLColAttribute function.
 
-  using namespace ignite::odbc::impl::binary;
   using namespace ignite::odbc::type_traits;
 
   std::string schema("database");
   std::string table("table");
   std::string column("column");
+
   ColumnMeta columnMeta(schema, table, column, JDBC_TYPE_VARCHAR,
                         Nullability::NULLABLE);
 
@@ -107,11 +108,11 @@ BOOST_AUTO_TEST_CASE(TestGetAttribute) {
   BOOST_CHECK(found);
   BOOST_CHECK_EQUAL(resVal, SqlTypeName::VARCHAR);
 
-  // fields SQL_COLUMN_PRECISION and SQL_DESC_SCALE are not tested 
+  // fields SQL_COLUMN_PRECISION and SQL_DESC_SCALE are not tested
   // for retrieving string values
 
   // test retrieving SQLLEN value
- 
+
   // test SQL_DESC_FIXED_PREC_SCALE
   found = columnMeta.GetAttribute(SQL_DESC_FIXED_PREC_SCALE, intVal);
   BOOST_CHECK(found);
@@ -151,6 +152,46 @@ BOOST_AUTO_TEST_CASE(TestGetAttribute) {
   found = columnMeta.GetAttribute(SQL_DESC_OCTET_LENGTH, intVal);
   BOOST_CHECK(found);
   BOOST_CHECK_EQUAL(intVal, SQL_NO_TOTAL);
+
+  // test SQL_DESC_NULLABLE
+  found = columnMeta.GetAttribute(SQL_DESC_NULLABLE, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, SQL_NULLABLE);
+
+  // test SQL_DESC_NUM_PREC_RADIX
+  found = columnMeta.GetAttribute(SQL_DESC_NUM_PREC_RADIX, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, 0);
+
+  // test SQL_DESC_PRECISION
+  found = columnMeta.GetAttribute(SQL_DESC_PRECISION, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, SQL_NO_TOTAL);
+
+  // test SQL_DESC_SCALE
+  found = columnMeta.GetAttribute(SQL_DESC_SCALE, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, -1);
+
+  // test SQL_DESC_SEARCHABLE
+  found = columnMeta.GetAttribute(SQL_DESC_SEARCHABLE, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, SQL_PRED_BASIC);
+
+  // test SQL_DESC_UNNAMED
+  found = columnMeta.GetAttribute(SQL_DESC_UNNAMED, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, SQL_NAMED);
+
+  // test SQL_DESC_UNSIGNED
+  found = columnMeta.GetAttribute(SQL_DESC_UNSIGNED, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, SQL_TRUE);
+
+  // test SQL_DESC_UPDATABLE
+  found = columnMeta.GetAttribute(SQL_DESC_UPDATABLE, intVal);
+  BOOST_CHECK(found);
+  BOOST_CHECK_EQUAL(intVal, SQL_ATTR_READWRITE_UNKNOWN);
 }
 
 
