ca0b188d7eb3247b434cf4b2c465629b1bcd57ec
Alina (Xi) Li <alinal@bitquilltech.com>
2022-06-15 13:02:53 -0700

    [AD-794] update mac build script to generate code coverage

--- build_mac_debug64.sh
+++ build_mac_debug64.sh
@@ -3,6 +3,7 @@ BUILD_TYPE=Debug
 PROJECT_DIR=$(pwd)
 DRIVER_BIN_DIR="$PROJECT_DIR/build/odbc/bin"
 DRIVER_LOG_DIR="$PROJECT_DIR/build/odbc/logs"
+BOOST_TEST_CATCH_SYSTEM_ERRORS=no
 
 mkdir $DRIVER_LOG_DIR
 
@@ -26,5 +27,9 @@ if [ ! -f "$JDBC_DRIVER_FULLPATH" ]; then
 fi
 
 cd cmake-build64
-make -j 4
+if [ -n "$RUN_CODE_COVERAGE" ]; then
+    make ccov-all -j 4
+else
+    make -j 4
+fi
 cd ..
