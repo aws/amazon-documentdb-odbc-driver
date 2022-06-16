5e11ff340334bbeb476ed33e99aca6cb6c584633
Bruce Irschick <brucei@bitquilltech.com>
2022-06-16 11:31:33 -0700

    [AD-572] Update script to install driver in INI file.

--- scripts/register_driver_unix.sh
+++ scripts/register_driver_unix.sh
@@ -33,5 +33,5 @@ echo "FileUsage=0"               >> "$ODBC_LIB_PATH/ignite-odbc-install.ini"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   odbcinst -i -d -f "$ODBC_LIB_PATH/ignite-odbc-install.ini"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  export ODBCINST="$ODBC_LIB_PATH/ignite-odbc-install.ini"
+  cp "$ODBC_LIB_PATH/ignite-odbc-install.ini" /Library/ODBC/odbcinst.ini
 fi
