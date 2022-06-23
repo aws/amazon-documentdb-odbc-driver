6ef2a1f0d97628c0bc425d058467afd518ce4ea2
Alina (Xi) Li <alinal@bitquilltech.com>
2022-06-23 14:13:11 -0700

    [AD-781] get install_dependencies_mac to run check_dependencies_mac.sh

--- scripts/install_dependencies_mac.sh
+++ scripts/install_dependencies_mac.sh
@@ -1,10 +1,18 @@
-
+# get the current script directory
+PROJECT_DIR=$(pwd)
+SCRIPT_DIR="$PROJECT_DIR/scripts"
 
 # install dependencies via brew
 brew tap homebrew/services
 
 # unlink unix ODBC driver manager
 brew unlink unixodbc
+
+echo "TRACE calling check_dependencies_mac.sh"
+
+source $SCRIPT_DIR/check_dependencies_mac.sh
+
+echo ${apps_installed[*]}                   
 
 # install iODBC driver manager
 brew install libiodbc
