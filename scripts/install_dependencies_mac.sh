400694d1a43302bd434088fa3ac7f1e674d9d1ea
Alina (Xi) Li <alinal@bitquilltech.com>
2022-06-23 14:59:39 -0700

    [AD-781] enable auto install java on mac
    * call check_java_dependency_mac.sh from install_dependencies_mac.sh

--- scripts/install_dependencies_mac.sh
+++ scripts/install_dependencies_mac.sh
@@ -1,8 +1,12 @@
+# this script installs all required dependencies on Mac and sets the appropriate vars
+
+# TODO -AL-: may need to write to zshrc file for permanent vars storing
+
 # get the current script directory
 PROJECT_DIR=$(pwd)
 SCRIPT_DIR="$PROJECT_DIR/scripts"
 
-# install dependencies via brew
+# install formula dependencies via brew
 brew tap homebrew/services
 
 # unlink unix ODBC driver manager
@@ -10,6 +14,8 @@ brew unlink unixodbc
 
 echo "TRACE calling check_dependencies_mac.sh"
 
+# check if dependnecies are installed
+chmod +x $SCRIPT_DIR/check_dependencies_mac.sh
 source $SCRIPT_DIR/check_dependencies_mac.sh
 # num_apps, apps_installed and req_apps are variables from check_dependencies_mac.sh
 
@@ -28,4 +34,18 @@ else
         brew install ${req_apps[i]}
     fi
     done
+fi
+
+echo "TRACE calling check_java_dependency_mac.sh"
+
+# check if java dependency is installed
+chmod +x $SCRIPT_DIR/check_java_dependency_mac.sh
+source $SCRIPT_DIR/check_java_dependency_mac.sh
+# java_ver and java_not_installed are variables from check_dependencies_mac.sh
+
+if [[ "${java_not_installed}" -eq "1" ]]; then
+    echo "$java_ver is not installed, attempt to install it with brew."
+    # install JAVA dependency via brew
+    brew tap homebrew/cask-versions
+    brew install --cask $java_ver
 fi
