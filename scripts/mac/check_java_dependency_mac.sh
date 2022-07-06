c2f2015d2fe323632d2dfc1b5a23034df420822f
Alina (Xi) Li <alinal@bitquilltech.com>
2022-07-06 14:16:01 -0700

    [AD-781] add more draft for checking java dependency
    * made if statement checks more robust

--- scripts/mac/check_java_dependency_mac.sh
+++ scripts/mac/check_java_dependency_mac.sh
@@ -7,7 +7,10 @@ java_ver=corretto
 if [[ ! -z "${JAVA_HOME}" ]] && [[ -d "${JAVA_HOME}" ]] && [[ -d "${JAVA_HOME}/include" ]]; then
     # Test Java in path and has minimum version
 #   -AL- e.g., openjdk 18 2022-03-22
-    javaVersionString=`echo "${$(java --version)}" | head -1` # works in terminal but not in .sh 
+    javaVersionString=`echo "$(java --version)" | head -1` # works in terminal but not in .sh 
+    # -AL- tests
+    javaVersionString="java 1.2.3 2012-11-11"
+    echo "-AL- TRACE javaVersionString: $javaVersionString"
     # IFS=';' read -ra javaVersionStringArr <<< $javaVersionString # invalid -a option "read: bad option: -a"
 # In order to read in an array in zsh, read requires the option -A (instead of -a
 
@@ -16,14 +19,24 @@ if [[ ! -z "${JAVA_HOME}" ]] && [[ -d "${JAVA_HOME}" ]] && [[ -d "${JAVA_HOME}/i
     #javaVersionStringArr=($javaVersionString) # Reading a space-delimited string into an array
 
     read -a javaVersionStringArrary <<< "$javaVersionString" # Reading a space-delimited string into an array
-    javaVersion=${javaVersionStringArrary[2]}
-    IFS='.'; read -ra javaVersionArray <<< $javaVersion; unset IFS; # -AL- works, if javaVersion=18.1.2, then echo $javaVersionArr gives 18 1 2
+    echo "-al- trace javaVersionStringArrary[1]: ${javaVersionStringArrary[1]}"
+    javaVersion=${javaVersionStringArrary[1]}
+    javaVersionArray=(${javaVersion//./ })
+    #IFS="."; read -ra javaVersionArray <<< $javaVersion; unset IFS; # -AL- works, if javaVersion=18.1.2, then echo $javaVersionArr gives 18 1 2
+    # ^ does not work on bash, only zsh
+    echo "-al- trace javaVersionArray: $javaVersionArray"
+    echo "-al- trace javaVersionArray array length: ${#javaVersionArray[@]}"
+    echo "-al- trace javaVersionArray[0]: ${javaVersionArray[0]}"
     if [[ ${#javaVersionArray[@]} -ge 1 ]] && [[ ! -z ${javaVersionArray[0]} ]]; then
-        if [[ "1" -eq $javaVersionArray[0] ]] && [[ ! -z ${javaVersionArray[1]} ]]; then
-            if  [[ $javaVersionArray[1] -eq "8" || $javaVersionArray[1] -eq "9" ]]; then
+        echo "pass 1"
+        if [[ "1" -eq ${javaVersionArray[0]} ]] && [[ ! -z ${javaVersionArray[1]} ]]; then
+            echo "pass 2"
+            if  [[ ${javaVersionArray[1]} -eq "8" || ${javaVersionArray[0]} -eq "9" ]]; then
+                echo "pass 3"
                 jdkFound=1
             fi
-        elif [[ $javaVersionArray[0] -ge "10" ]]; then
+        elif [[ ${javaVersionArray[0]} -ge "10" ]]; then
+            echo "pass 4"
             jdkFound=1
         fi
     fi
