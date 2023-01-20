bff6f11f7cccf3abbef72adfe85049f16daad39e
Bruce Irschick <brucei@bitquilltech.com>
2023-01-19 16:55:52 -0800

    [AD-1040] Attempt to fix Mac build - #4.

--- scripts/install_dependencies_mac.sh
+++ scripts/install_dependencies_mac.sh
@@ -47,13 +47,13 @@ else
     echo "Installing the missing required dependencies"
     for (( i=0 ; i<$num_apps ; i++ )); 
     do
-    if [[ "${apps_installed[i]}" -eq "0" ]]; then
-        echo  "${req_apps[i]} is not installed, attempt to install it with brew."
-        brew install ${req_apps[i]}
-        if [[ "${req_apps[i]}" -eq "libiodbc"]]; then
-            brew link --overwrite ${req_apps[i]}
+        if [[ "${apps_installed[i]}" -eq "0" ]]; then
+            echo  "${req_apps[i]} is not installed, attempt to install it with brew."
+            brew install ${req_apps[i]}
+            if [[ "${req_apps[i]}" -eq "libiodbc" ]]; then
+                brew link --overwrite ${req_apps[i]}
+            fi
         fi
-    fi
     done
 fi
 
